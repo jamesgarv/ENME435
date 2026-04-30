a9de3a0187f3e08beed6a7d46bae93c5bfabd6a8
jamesgarv <161558924+jamesgarv@users.noreply.github.com>
2026-04-30 00:26:27 -0400

    Update smart_doorbell.py

--- HW7/smart_doorbell.py
+++ HW7/smart_doorbell.py
@@ -21,32 +21,32 @@ counter = 0
 
 while True:
   counter += 1
-  print()
-  print("Looped ", counter, " times")
-  print()
+  print(f"\nLooped {counter} times")
 
-  command = 'libcamera-still --width 1000 --height 720 -t 2100 --timelapse 1000 --nopreview -o test%d.jpg'
-  os.system(command + " > /dev/null 2>&1") #removes print statement everytime a picture is taken
-
+  os.system('libcamera-still --width 1000 --height 720 -t 2100 --timelapse 1000 --nopreview -o test%d.jpg > /dev/null 2>&1')
   print("Captured 1st & 2nd image for analysing")
 
   # Mask images
-  test1 = cv2.imread("test1.jpg")
-  test2 = cv2.imread("test2.jpg")
+  img1 = cv2.imread("test1.jpg")
+  img2 = cv2.imread("test2.jpg")
+
+  if img1 is None or img2 is None:
+    print("Camera failed to capture images, skipping...")
+    continue
+  
+  masked1, gray1 = mask_image(img1)
+  masked2, gray2 = mask_image(img2)
 
   threshold = 50
   detector_total = np.uint64(0)
   detector = np.zeros((gray2.shape[0], gray2.shape[1]), dtype="uint8")
 
   # pixel by pixel comparison
-  for i in range(0, gray2.shape[0]):
-    for j in range(0, gray2.shape[1]):
-      if abs(int(gray2[i, j]) - int(gray1[i, jj])) > threshold:
-        detector[i, j] = 255
+  detector = np.where(np.abs(gray2.astype(int) - gray1.astype(int)) > threshold, 255, 0).astype("uint8")
 
   # sum detector array
-  detector_total = np.uint64(np.sum)detector))
-  print("detector_total = " detector_total)
+  detector_total = np.uint64(np.sum(detector))
+  print("detector_total = ", detector_total)
   print()
 
   if detector_total > 30000:
@@ -55,41 +55,36 @@ while True:
     # define unique name for a new video
     timestr = time.strftime("doorbell-%Y%m%d-%H%M%S")
 
-  command2 = "libcamera-vid --width 1000 --height 720 --framerate 30 -t 15000 -o " + timestr + ".h264"
-  os.system(command2)
+    command2 = "libcamera-vid --width 1000 --height 720 --framerate 30 -t 15000 -o " + timestr + ".h264"
+    os.system(command2)
 
-  print("Finished recording...converting to mp4")
+    print("Finished recording...converting to mp4")
 
-  command3 = f"ffmpeg -r 30 -i {timestr}.h264 -c:v copy {timestr}.mp4" # MP4Box was not available so using ffmpeg instead
-  os.system(command3)
+    command3 = f"ffmpeg -r 30 -i {timestr}.h264 -c:v copy {timestr}.mp4" # MP4Box was not available so using ffmpeg instead
+    os.system(command3)
 
-  print("Finished converting file, available for viewing")
+    print("Finished converting file, available for viewing")
 
-  # write masked images to file
-  cv2.imwrite("gray1.jpg", gray1)
-  cv2.imwrite("gray2.jpg", gray2)
-  cv2.imwrite("masked1.jpg", masked1)
-  cv2.imwrite("masked2.jpg", masked2)
+    # write masked images to file
+    cv2.imwrite("gray1.jpg", gray1)
+    cv2.imwrite("gray2.jpg", gray2)
+    cv2.imwrite("masked1.jpg", masked1)
+    cv2.imwrite("masked2.jpg", masked2)
 
-  # Upload video file to the cloud
-  fullDirectory = '/home/pi/SmartDoorbell/' + timestr + '.mp4'
-  command4 = 'sudo /home/pi/dropbox_uploader.sh upload ' + fullDirectory + ' /'
-  os.system(command4)
+    # Upload video file to the cloud
+    fullDirectory = '/home/pi/SmartDoorbell/' + timestr + '.mp4'
+    command4 = 'sudo /home/pi/dropbox_uploader.sh upload ' + fullDirectory + ' /'
+    os.system(command4)
 
-  # send email to user with images
-  password = 'wlxyjdlovicufodc'
-  yag = yagmail.SMTP('westonspi4@gmail.com', password)
+    # send email to user with images
+    password = 'raspberrypi'
+    yag = yagmail.SMTP('jamesgarvpi@gmail.com', password)
 
-  f_time = datetime.now().strftime('%a %d %b @ %H:%M')
-  images = ['test0.jpg', 'test1.jpg', 'gray1.jpg', 'gray2.jpg', 'masked1.jpg', 'masked2.jpg']
-  yag.send(to = 'wfuhr@terpmail.umd.edu', subject = 'Smart Doorbell recording from: ' + f_time, 
-           contents = "Smart Doorbell images: " + f_time, attachments = images)
+    f_time = datetime.now().strftime('%a %d %b @ %H:%M')
+    images = ['test1.jpg', 'test2.jpg', 'gray1.jpg', 'gray2.jpg', 'masked1.jpg', 'masked2.jpg']
+    yag.send(to = 'jgarvey5@terpmail.umd.edu', subject = 'Smart Doorbell recording from: ' + f_time, 
+             contents = "Smart Doorbell images: " + f_time, attachments = images)
     print("Email Delivered")
 
-else:
+  else:
     print("Nothing detected")
-
-# Read in Image
-#test1 = cv2.imread("DoorBellAlign3.jpg") # First Image take
-test1 = cv2.imread("DoorBellAlign2.jpg") # Second Test Image
-test1 = cv2.rotate(test1, cv2.ROTATE_90_CLOCKWISE)
